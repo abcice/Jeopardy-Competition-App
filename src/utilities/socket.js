2f4150ef45977661a93839123e018b1b028a30c8
Mohamed Meshaima <mohamedoaderu@gmail.com>
2025-09-15 16:06:35 +0300

    dot

--- src/utilities/socket.js
+++ src/utilities/socket.js
@@ -1,8 +1,9 @@
 // ✅ src/utilities/socket.js
 import { io } from "socket.io-client";
 import dotenv from 'dotenv';
+dotenv.config()
 
-const socket = io(import.meta.env.VITE_API_URL || "/", {
+const socket = io(process.env.VITE_API_URL, {
   autoConnect: false,
   withCredentials: true,
   transports: ["websocket"],
