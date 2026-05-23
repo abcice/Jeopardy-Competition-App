f5314137fde1ac50d0bf9189c8504a9adf713c59
Mohamed <mohamedoaderu@gmail.com>
2026-05-23 13:57:03 +0300

    Finish updates and cleanup

--- src/utilities/socket.js
+++ src/utilities/socket.js
@@ -1,8 +1,11 @@
 // ✅ src/utilities/socket.js
 import { io } from "socket.io-client";
+import dotenv from 'dotenv';
 
 const socket = io(import.meta.env.VITE_API_URL || "http://localhost:3000", {
   autoConnect: false,
+  withCredentials: true,
+  transports: ["websocket"],
 });
 
 export default socket;
